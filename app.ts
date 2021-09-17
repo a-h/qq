5cf29ff2cd54f81175ae28bb617156d37b27b57f
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-09-17 12:07:52 +0100

    added button click support

--- app.ts
+++ app.ts
@@ -1,16 +1,40 @@
-import { App, GenericMessageEvent } from '@slack/bolt';
+import { App, SlackEventMiddlewareArgs } from '@slack/bolt';
 
 const app = new App({
         token: process.env.SLACK_BOT_TOKEN,
         signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-app.message('hello', async params => {
+app.message('hello', async (params: SlackEventMiddlewareArgs<"message">) => {
         console.log('received message');
         if (params.message.subtype === undefined) {
-                const msg = params.message as GenericMessageEvent;
-                await params.say(`Hey there <@${msg.user}>!`);
+                await params.say({
+                        blocks: [
+                                {
+                                        "type": "section",
+                                        "text": {
+                                                "type": "mrkdwn",
+                                                "text": `Hey there <@${params.message.user}>!`
+                                        },
+                                        "accessory": {
+                                                "type": "button",
+                                                "text": {
+                                                        "type": "plain_text",
+                                                        "text": "Click Me"
+                                                },
+                                                "action_id": "button_click"
+                                        }
+                                }
+                        ],
+                        text: `Hey there <@${params.message.user}>!`
+                });
         }
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
